412b9d7181c6130d67d631fcbe4add533771d92f
EgoTkachenco <egortkachenco@gmail.com>
2019-11-25 16:14:49 +0300

    Room socket events add

--- socket/room_namespace.js
+++ socket/room_namespace.js
@@ -2,39 +2,39 @@ exports.createNameSpace = (io) => {
 		IO = io;
 		nsp = io.of('/room');
 		nsp.on('connection', socket => {
-			let roomID = '';
-			
+			let roomID = null;
 			socket.on('create-room', (data, fn) => {
+				socket.join(data);
 				roomID = data;
-				socket.join(roomID);
 				fn('Room Created ', data)
 			});
 
 			socket.on('connect-room', (roomId, fn) => {
 				if(io.nsps['/room'].adapter.rooms[roomId] && IO.nsps['/room'].adapter.rooms[roomId].length > 1) {
 					console.log('Cancel connect');
-					socket.emit('socket-error', 'Too many users in room already');
+					socket.emit('message', 'Too many users in room already');
 				} else {
+					roomID = roomId
 					socket.join(roomId);
-					roomID = roomId;
-					fn('Joined to room');
-					nsp.to(roomID).emit('userJoin');
-					socket.to(roomID).emit('start-video');
+					fn(true);
+					nsp.to(roomId).emit('message', 'Joined to room', 'success');
+					socket.to(roomId).emit('partner-connected');
+					socket.emit('get-invited-user-name');
+					socket.emit('start-video');
+
+					
 				};
 				
 			});
 
 			socket.on('code-changed', (data) => {
-				socket.to(roomID).emit('new-code', data);
+				console.log(data);
+				nsp.to(roomID).emit('new-code', data);
+			});			
+			socket.on('description-changed', (data) => {
+				console.log(roomID);
+
+				socket.to(roomID).emit('new-desc', data);
 			});
-			socket.on('send-invite-offer', (data) => {
-				socket.to(roomID).emit('invite-offer', data);
-				socket.emit('message', 'sender');
-			});
-			socket.on('send-answer-offer', (offer) =>{
-				socket.to(roomID).emit('answer-offer', offer);
-			})
-			
-			
 		})
 }
