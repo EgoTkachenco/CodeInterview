6b8bfed41951a16d985144a684f53dad5467baf7
oleskostyniuk <kostyniuk.oles@gmail.com>
2019-12-01 22:01:41 +0200

    Change Database to local

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ if(!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined');
   process.exit(1);
 }
-// 'mongodb://localhost/CodeInterview'
-
+// ''
+// mongodb+srv://Admin:qweasd123@cluster0-yzodb.mongodb.net/CodeInterview?retryWrites=true&w=majority
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Admin:qweasd123@cluster0-yzodb.mongodb.net/CodeInterview?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+
+mongoose.connect('mongodb://localhost/CodeInterview', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then(() => {
         console.log('Successfully connected to Mongo database');
     })
